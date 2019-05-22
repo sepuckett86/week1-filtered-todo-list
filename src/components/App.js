2d8655776ad988ca109ea2f6c794494c6d2d0206
sepuckett86 <sepuckett86@gmail.com>
2019-05-22 09:50:44 -0700

    add Header component and wire up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
 import Component from './Component.js';
+import Header from './Header.js';
 
 class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+        const main = dom.querySelector('main');
+
+        // Header
+        const headerComponent = new Header();
+        const headerComponentDOM = headerComponent.renderDOM();
+        dom.insertBefore(headerComponentDOM, main);
+
+        return dom;
+    }
     renderTemplate() {
         return /*html*/ `
             <div>
