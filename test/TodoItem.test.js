6d2f6e0152e98563976589f5e14d03744c0c467f
sepuckett86 <sepuckett86@gmail.com>
2019-05-22 10:18:26 -0700

    add another test for TodoItem without checked/

--- test/TodoItem.test.js
+++ test/TodoItem.test.js
@@ -3,21 +3,41 @@ const test = QUnit.test;
 
 QUnit.module('TodoItem');
 
-test('generate list item html', assert => {
+test('generate list item html with checked', assert => {
     // Arrange
     const todo = {
         task: 'Go for a walk in the forest',
         completed: true
     };
-    let checked = '';
-    if(todo.completed) {
-        checked = ' ' + 'checked';
-    }
+    
     const expected = /*html*/ `
         <li>
             <label>
                 ${todo.task}
-                <input type="checkbox" name="todo"${checked}>
+                <input type="checkbox" name="todo" checked>
+            </label>
+        </li>
+    `;
+    // Act
+
+    const todoItemComponent = new TodoItem({ todo });
+    const html = todoItemComponent.renderTemplate();
+
+    // Assert
+    assert.htmlEqual(html, expected);
+});
+
+test('generate list item html without checked', assert => {
+    // Arrange
+    const todo = {
+        task: 'Go for a walk in the forest',
+        completed: false
+    };
+    const expected = /*html*/ `
+        <li>
+            <label>
+                ${todo.task}
+                <input type="checkbox" name="todo">
             </label>
         </li>
     `;
