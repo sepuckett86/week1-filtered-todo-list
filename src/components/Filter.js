161b4739596d770afba36ced8ea4f0e51b023e4b
sepuckett86 <sepuckett86@gmail.com>
2019-05-23 15:45:39 -0700

    wrap a form around inputs to enable full functionality for filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,23 +2,25 @@ import Component from './Component.js';
 
 class Filter extends Component {
     render() {
-        const dom = this.renderDOM();
+        const form = this.renderDOM();
         
         const onFilter = this.props.onFilter;
 
-        const input = dom.querySelector('input');
-
-        input.addEventListener('input', () => {
+        form.addEventListener('input', () => {
+            const formData = new FormData(form);
+            const text = formData.get('filter');
+            const radio = formData.get('radio');
             onFilter({
-                text: input.value
+                text,
+                radio
             });
         });
 
-        return dom;
+        return form;
     }
     renderTemplate() {
         return /*html*/ `
-            <div id="filter">
+            <form id="filter">
                 <label id="text-filter">Filter: 
                     <input name="filter" type="text">
                 </label>
@@ -26,12 +28,12 @@ class Filter extends Component {
                     <input name="radio" type="radio" value="all" checked>
                 </label>
                 <label>Completed
-                    <input name="radio" type="radio" value="done">
+                    <input name="radio" type="radio" value="completed">
                 </label>
                 <label>Not Done
                     <input name="radio" type="radio" value="not-done">
                 </label>
-            </div>
+            </form>
         `;
     }
 }
